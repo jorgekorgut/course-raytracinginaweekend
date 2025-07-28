4a05f2beea844f531f2f463ef3a6190e02e27583
Jorge Korgut Junior <jorge.korgut-junior@insa-lyon.fr>
2025-07-28 17:12:35 +0200

    Finish

--- camera.h
+++ camera.h
@@ -15,10 +15,19 @@ public:
   int max_depth = 10;         // Maximum number of ray bounces into scene
   double vfov = 90;           // Vertical view angle (field of view)
 
+  point3 lookfrom = point3(0, 0, 0); // Point camera is looking from
+  point3 lookat = point3(0, 0, -1);  // Point camera is looking at
+  vec3 vup = vec3(0, 1, 0);          // Camera-relative "up" direction
+
+  double defocus_angle = 0; // Variation angle of rays through each pixel
+  double focus_dist =
+      10; // Distance from camera lookfrom point to plane of perfect focus
+
   void render(const hittable &world, const std::string &image_name) {
     initialize();
 
     // Render the image
+    
     for (int j = 0; j < image_height; j++) {
       std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
                 << std::flush;
@@ -51,27 +60,38 @@ private:
   point3 pixel00_loc;         // Location of pixel 0, 0
   vec3 pixel_delta_u;         // Offset to pixel to the right
   vec3 pixel_delta_v;         // Offset to pixel below
+
+  vec3 u, v, w; // Camera frame basis vectors
   std::vector<unsigned char> image;
+
+  vec3 defocus_disk_u; // Defocus disk horizontal radius
+  vec3 defocus_disk_v; // Defocus disk vertical radius
 
   void initialize() {
     image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
-    center = point3(0, 0, 0);
+    center = lookfrom;
     image = std::vector<unsigned char>(image_width * image_height * channels);
     pixel_samples_scale = 1.0 / samples_per_pixel;
 
     // Camera
-    auto focal_length = 1.0;
     auto theta = degrees_to_radians(vfov);
     auto h = std::tan(theta / 2);
-    auto viewport_height = 2 * h * focal_length;
+    auto viewport_height = 2 * h * focus_dist;
     auto viewport_width =
         viewport_height * (double(image_width) / image_height);
 
+    // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
+
     // Calculate the vectors across the horizontal and down the vertical
     // viewport edges.
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    vec3 viewport_u =
+        viewport_width * u; // Vector across viewport horizontal edge
+    vec3 viewport_v =
+        viewport_height * -v; // Vector down viewport vertical edge
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     pixel_delta_u = viewport_u / image_width;
@@ -79,9 +99,15 @@ private:
 
     // Calculate the location of the upper left pixel.
     auto viewport_upper_left =
-        center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
 
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Calculate the camera defocus disk basis vectors.
+    auto defocus_radius =
+        focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+    defocus_disk_u = u * defocus_radius;
+    defocus_disk_v = v * defocus_radius;
   }
 
   ray get_ray(int i, int j) const {
@@ -92,10 +118,16 @@ private:
     auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) +
                         ((j + offset.y()) * pixel_delta_v);
 
-    auto ray_origin = center;
+    auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
     auto ray_direction = pixel_sample - ray_origin;
 
     return ray(ray_origin, ray_direction);
+  }
+
+  point3 defocus_disk_sample() const {
+    // Returns a random point in the camera defocus disk.
+    auto p = random_in_unit_disk();
+    return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
   }
 
   vec3 sample_square() const {
